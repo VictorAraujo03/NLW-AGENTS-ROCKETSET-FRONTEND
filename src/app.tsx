63a0793c33f777c48016111da3918eb42efa8e18
Viter4 <thevictoraraujo7@gmail.com>
2025-07-10 23:42:48 -0300

    docs: add readme

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room.tsx'
+import { Room } from './pages/room.tsx'
+
+const queryClient = new QueryClient()
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
+}
